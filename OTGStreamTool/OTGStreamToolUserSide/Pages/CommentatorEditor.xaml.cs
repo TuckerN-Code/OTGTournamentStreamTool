7a50c67960d88f413c5918fae1e03b34568ed64b
TuckerN-Code <nultytucker@gmail.com>
2021-11-18 14:57:30 -0800

    Created new WPF pages
    Created 2 new wpf pages, player editor and commentator editor

---
+++ OTGStreamTool/OTGStreamToolUserSide/Pages/CommentatorEditor.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OTGStreamToolUserSide.Pages
+{
+    /// <summary>
+    /// Interaction logic for CommentatorEditor.xaml
+    /// </summary>
+    /// 
+    //USE DLLs TO TRANSFER DATA PLS PLS PLS
+    public partial class CommentatorEditor : Page
+    {
+        public string Comm1 { get; set; }
+        public CommentatorEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
